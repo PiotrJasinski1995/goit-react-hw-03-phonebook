c588bc2be65566e48b8cbfef31e566c9577df87c
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2024-01-29 01:05:19 +0100

    feat: implement saving contacts in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,37 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    let localContacts = [];
+
+    try {
+      localContacts = JSON.parse(localStorage.getItem('contacts'));
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+
+    if (localContacts?.length > 0) {
+      this.setState({
+        contacts: localContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    const prevContacts = prevState.contacts;
+
+    if (contacts?.length > 0) {
+      if (contacts.length !== prevContacts.length) {
+        console.log('Local storage changed');
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+      }
+    } else {
+      console.log('Remove from local storage');
+      localStorage.removeItem('contacts');
+    }
+  }
+
   handleFormSubmit = (name, number) => {
     const { contacts } = this.state;
     const nameContacts = contacts.map(contact => contact.name.toLowerCase());
